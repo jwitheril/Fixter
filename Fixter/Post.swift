586cbe9a1f65c2253442767ae62c3c2830877822
Jim Witheril <jwitheril@austin.rr.com>
2015-10-01 11:00:28 -0500

    first commit

---
+++ Fixter/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Fixter
+//
+//  Created by Jim & Lisa on 9/30/15.
+//  Copyright (c) 2015 Jim Witheril. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+   
+}
